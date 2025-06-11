b77f2b687f7821bb8c9db1a8e05b13993db1d894
killi1812 <cvok.fran@gmail.com>
2025-06-11 13:53:09 +0200

    Sve radi

--- DokumentApplication/src/dokumentapplication/DokumentApplication.java
+++ DokumentApplication/src/dokumentapplication/DokumentApplication.java
@@ -1,14 +1,10 @@
 package dokumentapplication;
 
 import generated.UserInfo;
-import generated.ObjectFactory;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -17,22 +13,34 @@ import org.xml.sax.SAXException;
 public class DokumentApplication {
 
     public static void main(String[] args) {
+        String xmlFilePath, xsdFilePath;
 
-        String xmlFilePath = "dokumenti.xml";
-        // Putanja do XSD sheme mora biti relativna u odnosu na korijen projekta
-        String xsdFilePath = "xml-resources/jaxb/DokumentXsd/dokumenti.xsd";
+        if (args.length == 0) {
+            System.err.println("GREŠKA: Nije zadana putanja do XML datoteke.");
+            System.err.println("Uporaba: java -jar DokumentApplication.jar <putanja_do_xml_datoteke>");
+            xmlFilePath = "dokumenti.xml";
+            xsdFilePath = "dokumenti.xsd";
+            System.exit(1);
+        } else {
+            xmlFilePath = args[1];
+            xsdFilePath = args[0];
+        }
 
-
+        // Stvaranje sheme iz učitanog 'InputStream-a'
         // =====================================================================
         //              Validacija XML datoteke prema XSD shemi
         // =====================================================================
-        
         System.out.println("--- Validacija XML-a prema XSD-u ---");
         try {
             JAXBContext jc = JAXBContext.newInstance(UserInfo.class);
-            
+
             SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = sf.newSchema(new File(xsdFilePath));
+            File xsdFile = new File(xsdFilePath);
+            if (!xsdFile.exists()) {
+                System.err.println("GREŠKA: XSD shema nije pronađena na putanji: " + xsdFilePath);
+                System.exit(1);
+            }
+            Schema schema = sf.newSchema(xsdFile);
             Unmarshaller unmarshaller = jc.createUnmarshaller();
 
             unmarshaller.setSchema(schema);
@@ -41,15 +49,16 @@ public class DokumentApplication {
                 System.out.println("  - LINIJA:    " + event.getLocator().getLineNumber());
                 System.out.println("  - KOLONA:  " + event.getLocator().getColumnNumber());
                 System.out.println("  - PORUKA: " + event.getMessage());
-                return false; 
+                return false;
             });
-
 
             unmarshaller.unmarshal(new File(xmlFilePath));
             System.out.println("VALIDACIJA USPJEŠNA: Datoteka '" + xmlFilePath + "' je u skladu s XSD shemom.");
 
         } catch (JAXBException | SAXException ex) {
             System.out.println("\nVALIDACIJA NEUSPJEŠNA: XML datoteka sadrži greške.");
+            System.out.println(ex);
+
         }
         System.out.println("---------------------------------------");
     }
